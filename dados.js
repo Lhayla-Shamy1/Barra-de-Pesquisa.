1120c0b7dc50f01bbc9127a3c89c5ef2d284eef4
Lhayla Shamy Santos Fonseca <160618204+Lhayla-Shamy1@users.noreply.github.com>
2025-03-23 15:02:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,16 @@
+let dados = [
+    {   nome : "Lamen coreano",
+        descricao : "Macarrão instantâneo com sabores picantes e variados, como kimchi e frutos do mar. É uma refeição rápida e saborosa, perfeita para quem busca praticidade.",
+        link : "https://www.hachi8.com.br/alimentos/lamen-coreano-super-picante-buldak-kimchi-big-bowl-samyang-105g"
+    },
+    {
+        nome : "Topokki",
+        descricao : "Bolinhos de arroz cilíndricos cozidos em um molho picante e adocicado à base de gochujang (pasta de pimenta coreana). É um prato popular de rua na Coreia do Sul.",
+        link : "https://comidasasiaticas.com/tteokbokki-topokki-como-fazer-prato-coreano/"
+    },
+    {
+        nome : "Kimchi",
+        descricao : " Conserva de vegetais fermentados, geralmente acelga e nabo, com temperos picantes. É um acompanhamento essencial na culinária coreana, rico em probióticos e sabor único.",
+        link : "https://www.bbc.com/portuguese/articles/cnerglzzmpno#:~:text=Para%20preparar%20o%20kimchi%2C%20a,ambiente%20e%20depois%20na%20geladeira."
+    }
+];
